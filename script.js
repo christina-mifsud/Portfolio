14a6df8163c112bd9cc8c22c4c69e452755ee297
christina-mifsud <christina.mifsud@gmail.com>
2024-04-01 19:37:07 +0100

    mouse point animation

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ toggleButton.addEventListener("click", function () {
   }
 });
 
-///////////////////////////////////////////////////
+// on-load overlay
 
 setTimeout(function () {
   const hideOverlay = sessionStorage.getItem("hideOverlay");
@@ -32,3 +32,44 @@ function off() {
   document.getElementById("on-load-overlay").style.display = "none";
   sessionStorage.setItem("hideOverlay", "true");
 }
+
+// mouse pointer animation - following tutorial found here: https://www.youtube.com/watch?v=7eE8xPyXSR4
+
+const coords = { x: 0, y: 0 };
+const squares = document.querySelectorAll(".mouse-square");
+
+const colors = ["#f2511b"];
+
+squares.forEach(function (square, index) {
+  square.x = 0;
+  square.y = 0;
+  square.style.backgroundColor = colors;
+});
+
+window.addEventListener("mousemove", function (e) {
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+});
+
+function animateSquares() {
+  let x = coords.x;
+  let y = coords.y;
+
+  squares.forEach(function (square, index) {
+    square.style.left = x - 12 + "px";
+    square.style.top = y - 12 + "px";
+
+    square.style.scale = (squares.length - index) / squares.length;
+
+    square.x = x;
+    square.y = y;
+
+    const nextSquare = squares[index + 1] || squares[0];
+    x += (nextSquare.x - x) * 0.3;
+    y += (nextSquare.y - y) * 0.3;
+  });
+
+  requestAnimationFrame(animateSquares);
+}
+
+animateSquares();
